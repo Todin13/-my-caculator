f814340d524fe566bca6a9aea414bc5d97abd472
Todin13 <thomasodin13@gmail.com>
2023-02-15 18:21:10 +0100

    Add pow function

--- main.sh
+++ main.sh
@@ -17,8 +17,14 @@ sub(){
 
 }
 
+pow(){
+	t=$1
+	g=$2
+	echo "Power : $t ** $g = $((t**g))"
+}
+
 echo "Your choices :"
-echo -e "sum\nReal Division\nsub\n"
+echo -e "sum\nReal Division\nsub\npow\n"
 read -p "Enter your choice : " ch
 case $ch in 
 	1|"sum")read -p "Number a to add : " a ;
@@ -33,6 +39,10 @@ case $ch in
                 read -p "Number b to substract by : " b  ; 
                 sub a b
 	;;
+	4|"pow")read -p "Number a to be powered : " a ;
+                read -p "Number b to powered by : " b  ;
+                pow a b
+        ;;
 esac
 
 
