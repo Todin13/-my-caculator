1e5ac746e1edf981b5b556749a3054300b6427d9
Todin13 <thomasodin13@gmail.com>
2023-02-15 18:17:21 +0100

    Add sub function

--- main.sh
+++ main.sh
@@ -10,9 +10,15 @@ realDiv(){
 	echo "Real Division : $o / $p = $((o/p))"
 }
 
+sub(){
+	e=$1
+	z=$2
+	echo "Substraction : $e - $z = $((e-z))"
+
+}
 
 echo "Your choices :"
-echo -e "sum\nReal Division\n"
+echo -e "sum\nReal Division\nsub\n"
 read -p "Enter your choice : " ch
 case $ch in 
 	1|"sum")read -p "Number a to add : " a ;
@@ -22,6 +28,10 @@ case $ch in
 	2|"Real Division"|"real division")read -p "Number a to be divide : " a ;
                				  read -p "Number b to divide by : " b  ; 
                				  realDiv a b
+	;;
+	3|"sub")read -p "Number a to be substract : " a ;
+                read -p "Number b to substract by : " b  ; 
+                sub a b
 	;;
 esac
 
