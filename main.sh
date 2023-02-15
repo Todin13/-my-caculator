14b6e5cf8b354ddbf520c6ac2452753ddaa8c1bf
Todin13 <thomasodin13@gmail.com>
2023-02-15 18:10:42 +0100

    Add realDiv function

--- main.sh
+++ main.sh
@@ -1,17 +1,27 @@
 sum(){
-  m=$1
-  n=$2
-  echo "Sum : $m + $n = $((m+n))"
+	m=$1
+	n=$2
+	echo "Sum : $m + $n = $((m+n))"
+}
+
+realDiv(){
+	o=$1
+	p=$2
+	echo "Real Division : $o / $p = $((o/p))"
 }
 
 
 echo "Your choices :"
-echo -e "sum\n"
-read -p "Enter your choices : " ch
+echo -e "sum\nReal Division\n"
+read -p "Enter your choice : " ch
 case $ch in 
 	1|"sum")read -p "Number a to add : " a ;
 	       	read -p "Number b to add : " b  ; 
 		sum a b
+	;;
+	2|"Real Division"|"real division")read -p "Number a to be divide : " a ;
+               				  read -p "Number b to divide by : " b  ; 
+               				  realDiv a b
 	;;
 esac
 
